4d500fb392388d4bb701d028d926e73457a116c3
Gabriel Portari <105023300+GabrielPortari@users.noreply.github.com>
2023-10-12 19:18:30 -0400

    Update YoutubeConfiguration.java

--- app/src/main/java/com/example/youtubeclone/helper/YoutubeConfiguration.java
+++ app/src/main/java/com/example/youtubeclone/helper/YoutubeConfiguration.java
@@ -3,6 +3,6 @@ package com.example.youtubeclone.helper;
 public class YoutubeConfiguration {
 
     public static String CANAL_ID = "@TheOffice";
-    public static String CHAVE_YOUTUBE_API = "AIzaSyAmtKuRZzYbZ7YjduQRrUYDZ2pYJLJ8ee8";
+    public static String CHAVE_YOUTUBE_API = "ENTER_API_KEY_HERE";
     public static String URL_BASE = "https://www.googleapis.com/youtube/v3/";
 }
